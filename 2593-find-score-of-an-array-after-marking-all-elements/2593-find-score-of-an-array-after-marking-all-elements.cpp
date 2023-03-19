a01ddc48be2eb4296552c87ffc271cdeab90f97c
Naman Jain <114653666+namanjain2602@users.noreply.github.com>
2023-03-19 11:01:03 +0530

    Time: 496 ms (40.00%), Space: 135 MB (20.00%) - LeetHub

---
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        long long ans = 0;
+        set<pair<int, int>> st;
+        for(int i = 0; i < nums.size(); i++){ st.insert({nums[i], i}); }
+        for(auto s: st){
+            if(nums[s.second]) {
+                nums[s.second] = 0;
+                if(s.second - 1 >= 0) nums[s.second - 1] = 0;
+                if(s.second + 1 < nums.size()) nums[s.second + 1] = 0;
+                ans += s.first;
+            }
+        }
+        return ans;
+    }
+};
